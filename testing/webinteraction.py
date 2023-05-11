c31d215a738321c9c0b531a361a78929620fc9f6
lindsey98 <kelseyliu1998@gmail.com>
2023-05-11 14:25:39 +0800

    m

---
+++ testing/webinteraction.py
@@ -0,0 +1,39 @@
+import time
+from xdriver.xutils.PhishIntentionWrapper import PhishIntentionWrapper
+from xdriver.xutils.WebInteraction import WebInteraction
+from xdriver.xutils.Logger import Logger
+from mmocr.utils.ocr import MMOCR
+from xdriver.xutils.forms.SubmissionButtonLocator import SubmissionButtonLocator
+from xdriver.XDriver import XDriver
+
+if __name__ == "__main__":
+    sleep_time = 5; timeout_time = 30
+    phishintention_cls = PhishIntentionWrapper()
+    mmocr_model = MMOCR(det=None,
+                        recog='ABINet',
+                        device='cuda',
+                        config_dir='/home/ruofan/git_space/phishing-research/web_interaction/xdriver3-open/configs/')
+    button_locator_model = SubmissionButtonLocator(
+        button_locator_config='/home/ruofan/git_space/phishing-research/web_interaction/xdriver3-open/xutils/forms/button_locator_models/config.yaml',
+        button_locator_weights_path='/home/ruofan/git_space/phishing-research/web_interaction/xdriver3-open/xutils/forms/button_locator_models/model_final.pth')
+
+    InteractionModel = WebInteraction(phishintention_cls=phishintention_cls,
+                                      mmocr_model=mmocr_model,
+                                      button_locator_model=button_locator_model,
+                                      interaction_depth=1)
+    Logger.set_debug_on()
+    XDriver.set_headless()
+    driver = XDriver.boot(chrome=True)
+    driver.set_script_timeout(timeout_time)
+    driver.set_page_load_timeout(timeout_time)
+    time.sleep(sleep_time)  # fixme: you have to sleep sometime, otherwise the browser will keep crashing
+
+    target = 'www.facebook.com'
+
+    driver.get(target, accept_cookie=True, click_popup=True)
+    benign, algo_time, total_time, \
+    redirection_evasion, no_verification = InteractionModel.get_benign(orig_url=driver.current_url(),
+                                                                       driver=driver)
+    print(benign)
+
+    driver.quit()
