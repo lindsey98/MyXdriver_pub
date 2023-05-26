1ec4b76ddf00a5f7d052e027423c0556ab220220
lindsey98 <51521323+lindsey98@users.noreply.github.com>
2023-05-26 19:32:41 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -229,7 +229,7 @@ esac
 ## MMOCR
 pip uninstall -y mmdet mmcv
 conda install -y cython==0.28.5
-pip install terminaltables Pillow==6.2.2
+pip install terminaltables Pillow
 pip install mmcv==0.6.2 mmcv-full
 pip install mmdet==2.2.1
 pip install mmocr==0.5.0
