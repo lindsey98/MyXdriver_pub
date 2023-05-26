a8f98ba1335bfbbbbb45c0c60ec9d038f484191b
lindsey98 <51521323+lindsey98@users.noreply.github.com>
2023-05-26 19:25:13 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -230,7 +230,7 @@ esac
 pip uninstall -y mmdet mmcv
 conda install -y cython==0.28.5
 pip install terminaltables Pillow==6.2.2
-pip install mmcv-full==1.5.0 
+pip install mmcv==1.5.0 mmcv-full
 pip install mmdet==2.23.0
 pip install mmocr==0.5.0
 
