6dea549c2f136029157b9b8c93c51d3ea7649636
lindsey98 <51521323+lindsey98@users.noreply.github.com>
2023-08-26 11:41:49 +0800

    Update formfill.py

--- testing/formfill.py
+++ testing/formfill.py
@@ -7,7 +7,7 @@ from xdriver.xutils.PhishIntentionWrapper import PhishIntentionWrapper
 import time
 
 if __name__ == '__main__':
-    orig_url = "https://reg.ebay.com/reg/PartialReg"
+    orig_url = "https://signup.ebay.com/pa/crte"
     sleep_time = 3; timeout_time = 60
     XDriver.set_headless()
     driver = XDriver.boot(chrome=True)
