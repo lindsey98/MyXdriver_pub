04b69b9cad7b05d357f242bec3856f6224c1fdfa
lindsey98 <51521323+lindsey98@users.noreply.github.com>
2023-09-12 21:37:46 +0800

    Update Form.py, dont use ocr model

--- xutils/forms/Form.py
+++ xutils/forms/Form.py
@@ -47,37 +47,37 @@ class Form():
         Logger.spit("Number of inputs = {}, Number of buttons = {}".format(len(self._inputs), len(self._buttons)),
                     debug=True, caller_prefix=Form._caller_prefix)
 
-    def erase_input_placeholder(self):
-        self._driver._invoke(self._driver.execute_script, '''
-            let inputslist = document.getElementsByTagName('input');
-            for (let i = 0; i <= inputslist.length; i++) {
-                let input = inputslist[i];
-                console.log(input);
+    # def erase_input_placeholder(self):
+    #     self._driver._invoke(self._driver.execute_script, '''
+    #         let inputslist = document.getElementsByTagName('input');
+    #         for (let i = 0; i <= inputslist.length; i++) {
+    #             let input = inputslist[i];
+    #             console.log(input);
 
-                try{
-                    let nodetag = input.tagName.toLowerCase();
-                    let etype = input.type;
-                    let location = get_loc(input);
-                    if (nodetag == "select" || etype == "submit" || etype == "button" || etype == "image" || etype == "reset" || etype == "radio" || etype == "checkbox" || etype == "hidden") {
-                        continue;
-                    }
-                    if (location[2] - location[0] <= 5 || location[3] - location[1] <= 5){
-                        continue;
-                    }
-                    // element obfuscation
-                    input.setAttribute('placeholder','');
-                }
-                catch(err){
-                    console.log(err);
-                    continue;
-                }
-            } 
-        '''
-                             )
+    #             try{
+    #                 let nodetag = input.tagName.toLowerCase();
+    #                 let etype = input.type;
+    #                 let location = get_loc(input);
+    #                 if (nodetag == "select" || etype == "submit" || etype == "button" || etype == "image" || etype == "reset" || etype == "radio" || etype == "checkbox" || etype == "hidden") {
+    #                     continue;
+    #                 }
+    #                 if (location[2] - location[0] <= 5 || location[3] - location[1] <= 5){
+    #                     continue;
+    #                 }
+    #                 // element obfuscation
+    #                 input.setAttribute('placeholder','');
+    #             }
+    #             catch(err){
+    #                 console.log(err);
+    #                 continue;
+    #             }
+    #         } 
+    #     '''
+    #                          )
 
     """
 		Reinitialize when the screenshot has changed
-	"""
+    """
 
     def button_reinitialize(self):
         # find clickable elements
@@ -143,12 +143,12 @@ class Form():
             self.erase_input_placeholder()
         elements_loc = list(map(lambda e: self._driver.get_location(e), elements))
 
-        cv_elements, cv_elements_dom, cv_elements_loc = self.get_input_elements_cv()  # also add the input elements reported from CV
-        for it, b in enumerate(cv_elements):
-            if b not in elements:
-                elements.append(b)
-                elements_dom.append(cv_elements_dom[it])
-                elements_loc.append(cv_elements_loc[it])
+        # cv_elements, cv_elements_dom, cv_elements_loc = self.get_input_elements_cv()  # also add the input elements reported from CV
+        # for it, b in enumerate(cv_elements):
+        #     if b not in elements:
+        #         elements.append(b)
+        #         elements_dom.append(cv_elements_dom[it])
+        #         elements_loc.append(cv_elements_loc[it])
 
         filter_elements = []
         filter_elements_dom = []
@@ -173,42 +173,42 @@ class Form():
                     visibilities.append(visible)
                     types.append(FE._etype)
                     locations.append(element_loc)
-                else:
-                    if visible:
-                        # Step2: use regex to match OCR result
-                        start_time = time.time()
-                        ocr_string_this_input = self._call_ocr(element_loc)
-                        end_time = time.time()
-                        Logger.spit("Run OCR on input element {} takes time: {:.4f}s".format(jj, end_time - start_time),
-                                    debug=True, caller_prefix=Form._caller_prefix)
-                        matched_rule = FE._decide_input_rule_given_str(ocr_string_this_input)
-                        if matched_rule is not None:
-                            filter_elements.append(element)
-                            filter_elements_dom.append(element_dom)
-                            rules.append(matched_rule)
-                            visibilities.append(visible)
-                            types.append(FE._etype)
-                            locations.append(element_loc)
+                # else:
+                #     if visible:
+                #         # Step2: use regex to match OCR result
+                #         start_time = time.time()
+                #         ocr_string_this_input = self._call_ocr(element_loc)
+                #         end_time = time.time()
+                #         Logger.spit("Run OCR on input element {} takes time: {:.4f}s".format(jj, end_time - start_time),
+                #                     debug=True, caller_prefix=Form._caller_prefix)
+                #         matched_rule = FE._decide_input_rule_given_str(ocr_string_this_input)
+                #         if matched_rule is not None:
+                #             filter_elements.append(element)
+                #             filter_elements_dom.append(element_dom)
+                #             rules.append(matched_rule)
+                #             visibilities.append(visible)
+                #             types.append(FE._etype)
+                #             locations.append(element_loc)
 
-                        else:
-                            # Step3: use regex to match the label near the input
-                            # 3.1 get previous sibling element
-                            closest_label = self._driver.get_element_prev_sibling(element)
-                            start_time = time.time()
-                            ocr_string_label = self._call_ocr_element(closest_label)
-                            end_time = time.time()
-                            Logger.spit("Run OCR on input element {} 's nearest label takes time: {:.4f}s".format(jj,
-                                                                                                                  end_time - start_time),
-                                        debug=True, caller_prefix=Form._caller_prefix)
-                            matched_rule = FE._decide_input_rule_given_str(ocr_string_label)
+                        # else:
+                        #     # Step3: use regex to match the label near the input
+                        #     # 3.1 get previous sibling element
+                        #     closest_label = self._driver.get_element_prev_sibling(element)
+                        #     start_time = time.time()
+                        #     ocr_string_label = self._call_ocr_element(closest_label)
+                        #     end_time = time.time()
+                        #     Logger.spit("Run OCR on input element {} 's nearest label takes time: {:.4f}s".format(jj,
+                        #                                                                                           end_time - start_time),
+                        #                 debug=True, caller_prefix=Form._caller_prefix)
+                        #     matched_rule = FE._decide_input_rule_given_str(ocr_string_label)
 
-                            if matched_rule is not None:
-                                filter_elements.append(element)
-                                filter_elements_dom.append(element_dom)
-                                rules.append(matched_rule)
-                                visibilities.append(visible)
-                                types.append(FE._etype)
-                                locations.append(element_loc)
+                        #     if matched_rule is not None:
+                        #         filter_elements.append(element)
+                        #         filter_elements_dom.append(element_dom)
+                        #         rules.append(matched_rule)
+                        #         visibilities.append(visible)
+                        #         types.append(FE._etype)
+                        #         locations.append(element_loc)
 
                             # else:
                             #     # 3.2 get PhishIntention reported labels
